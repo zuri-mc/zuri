00a84142bcead00b52cef913593f4a26220af930
AndreasHGK <andreaselbergs@outlook.com>
2022-12-21 23:44:39 +0100

    zuri/main.rs: Common twisted bruh moment

--- zuri/src/main.rs
+++ zuri/src/main.rs
@@ -42,6 +42,24 @@ fn main() {
         .run();
 }
 
+fn cursor_grab_system(
+    mut windows: ResMut<Windows>,
+    btn: Res<Input<MouseButton>>,
+    key: Res<Input<KeyCode>>,
+) {
+    let window = windows.get_primary_mut().unwrap();
+
+    if btn.just_pressed(MouseButton::Left) {
+        window.set_cursor_grab_mode(CursorGrabMode::Locked);
+        window.set_cursor_visibility(false);
+    }
+
+    if key.just_pressed(KeyCode::Escape) {
+        window.set_cursor_grab_mode(CursorGrabMode::None);
+        window.set_cursor_visibility(true);
+    }
+}
+
 fn setup(
     mut commands: Commands,
     mut wireframe_config: ResMut<WireframeConfig>,
