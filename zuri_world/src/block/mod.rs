d238289b0b660331627085d3a05692af0fcfe8ea
AndreasHGK <andreaselbergs@outlook.com>
2023-06-24 16:50:55 +0200

    zuri_world: Move RuntimeId to runtime_id.rs

--- zuri_world/src/block/mod.rs
+++ zuri_world/src/block/mod.rs
@@ -1,6 +1,5 @@
 use std::any::TypeId;
 use std::borrow::{Borrow, Cow};
-use std::cmp::Ordering;
 use std::collections::{BTreeMap, HashMap};
 use std::fmt::{Display, Formatter, Write};
 use std::hash::{Hash, Hasher};
@@ -8,15 +7,16 @@ use std::io;
 use std::ops::Deref;
 
 pub use sorted_vec::SortedSet;
-use thiserror::Error;
 
 pub use builder::{BlockBuilder, BlockMapBuilder};
+pub use runtime_id::*;
 pub use vanilla::AIR_ID;
 
 use crate::block::component::*;
 
 mod builder;
 pub mod component;
+mod runtime_id;
 mod vanilla;
 
 /// Holds all known block types and runtime blocks.
@@ -542,72 +542,5 @@ impl<'a> Display for PropertyValue<'a> {
             PropertyValue::Bool(v) => v.fmt(f),
             PropertyValue::Int(v) => v.fmt(f),
         }
-    }
-}
-
-/// A block runtime id.
-///
-/// Each combination of a unique block identifier and a set of properties mapped to one of the
-/// possible property values has its own runtime id. The amount of unique runtime ids each
-/// [BlockType] has corresponds with [BlockType::variant_count]. Runtime ids are also unique between
-/// different block types.
-#[derive(Debug, Copy, Clone, Eq, Ord, Hash)]
-pub struct RuntimeId(pub u32);
-
-/// Allows for types to be converted to a [RuntimeId] given a [BlockType].
-pub trait ToRuntimeId {
-    /// The error to return when conversion to a runtime id has failed.
-    type Err: std::error::Error;
-
-    /// Get the [RuntimeId] corresponding with the value of the type.
-    fn to_runtime_id(self, block_map: &BlockMap) -> Result<RuntimeId, Self::Err>;
-}
-
-/// Automatically implement [ToRuntimeId] when conversion is trivial and doesn't need [BlockMap].
-impl<T: Into<RuntimeId>> ToRuntimeId for T {
-    type Err = OutOfRangeError;
-
-    fn to_runtime_id(self, block_map: &BlockMap) -> Result<RuntimeId, Self::Err> {
-        let rid = self.into();
-        if rid >= block_map.runtime_ids() {
-            return Err(OutOfRangeError);
-        }
-        Ok(rid)
-    }
-}
-
-/// Returned when conversion to a runtime id results in a runtime id higher than the highest known
-/// one.
-#[derive(Debug, Error, Copy, Clone)]
-#[error("the runtime id is out of range")]
-pub struct OutOfRangeError;
-
-impl<T: Copy + Into<RuntimeId>> PartialEq<T> for RuntimeId {
-    fn eq(&self, other: &T) -> bool {
-        self.0 == other.clone().into().0
-    }
-}
-
-impl<T: Copy + Into<RuntimeId>> PartialOrd<T> for RuntimeId {
-    fn partial_cmp(&self, other: &T) -> Option<Ordering> {
-        self.0.partial_cmp(&other.clone().into().0)
-    }
-}
-
-impl Display for RuntimeId {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        <u32 as Display>::fmt(&self.0, f)
-    }
-}
-
-impl From<u32> for RuntimeId {
-    fn from(value: u32) -> Self {
-        Self(value)
-    }
-}
-
-impl From<RuntimeId> for u32 {
-    fn from(value: RuntimeId) -> Self {
-        value.0
     }
 }
