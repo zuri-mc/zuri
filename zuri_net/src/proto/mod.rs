50ddc60d149c16727809404c77965f3fb8b0492c
AndreasHGK <andreaselbergs@outlook.com>
2023-07-11 18:15:06 +0200

    Start working on support for 1.20.10

--- zuri_net/src/proto/mod.rs
+++ zuri_net/src/proto/mod.rs
@@ -6,5 +6,5 @@ pub mod packet;
 #[allow(clippy::all)] // todo: remove on cleanup.
 pub mod types;
 
-pub const CURRENT_PROTOCOL: i32 = 589;
-pub const CURRENT_VERSION: &str = "1.20.0";
+pub const CURRENT_PROTOCOL: i32 = 594;
+pub const CURRENT_VERSION: &str = "1.20.10";
